ee4cf1e0fd06bc6c72c313daf58b17e310fadbb8
unknown <eddiesongbusiness@gmail.com>
2024-07-15 20:47:18 -0400

    added hint and solution capabilities

--- app/helper/components/chatbot/chatbot.js
+++ app/helper/components/chatbot/chatbot.js
@@ -3,6 +3,7 @@
 import React, { useState } from "react";
 
 import { getChatbotResponse } from "../../apiservices/chatbotresponseservice";
+import { Hind_Guntur } from "next/font/google";
 
 export default function Chatbot(question) {
 
@@ -49,8 +50,36 @@ export default function Chatbot(question) {
             updateUserMessages({ user_message: user_message });
             const message = user_message;
             setUserMessage('');
-            const response = await getChatbotResponse(message);
+            try{
+                const response = await getChatbotResponse(message, question);
+                updateAssistantMessages({ assistant_message: response});
+            } catch(e) {
+                console.log(e);
+            }
+        }
+    };
+
+    const handleHint = async (event) => {
+        updateUserMessages({ user_message: "Give me a hint." });
+        const message = user_message;
+        setUserMessage('');
+        try{
+            const response = await getChatbotResponse(message, question);
             updateAssistantMessages({ assistant_message: response});
+        } catch(e) {
+            console.log(e);
+        }
+    };
+
+    const handleSolution = async (event) => {
+        updateUserMessages({ user_message: "Explain this problem to me." });
+        const message = user_message;
+        setUserMessage('');
+        try{
+            const response = await getChatbotResponse(message, question);
+            updateAssistantMessages({ assistant_message: response});
+        } catch(e) {
+            console.log(e);
         }
     };
 
@@ -61,13 +90,28 @@ export default function Chatbot(question) {
     };
 
     return (
-        <div className="bg-orange h-[700px] w-[200px] relative right-[4%] top-[6%]" style={{ overflowY: 'auto'}}>
-            <div className="flex-1 bg-black h-[500px] flex flex-col justify-end text-white p-[5px]">
+        <div className="bg-orange h-[700px] w-[300px] relative right-[2%] top-[6%]" style={{ overflowY: 'auto'}}>
+            <div className="flex-1 bg-black h-[500px] flex flex-col justify-end text-white p-[5px] w-full">
                 {messages.map((msg, index) => (
                     <div key={index} className={`message ${msg.role}`}>
                         <strong>{msg.role}: </strong>{msg.message}
                     </div>
                 ))}
+            </div>
+            <br></br>
+            <div>
+                <button
+                    className="w-half p-2 border border-gray-300 rounded"
+                    onClick={handleHint}
+                >
+                    Give me a hint.
+                </button>
+                <button 
+                    className="w-half p-2 border border-gray-300 rounded"
+                    onClick={handleSolution}
+                >
+                    Give me the solution.
+                </button>
             </div>
             <form onSubmit={handleSubmit} className="mt-4">
                 <textarea

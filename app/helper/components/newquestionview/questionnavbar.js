88e716d6d6ef4bce4e95d62bf671fc01798b9218
unknown <eddiesongbusiness@gmail.com>
2024-08-01 19:16:43 -0400

    further navbar changes

--- app/helper/components/newquestionview/questionnavbar.js
+++ app/helper/components/newquestionview/questionnavbar.js
@@ -39,15 +39,19 @@ export default function QuestionNavBar({ review, questions, quizName }) {
     }
 
     return (
-        <div className='h-[90%] w-[250px] bg-appleGray6 relative top-[40px] left-[50px] flex flex-col justify-between items-center pt-[25px] pb-[50px] rounded-[25px]'>
-            <h5 className='w-[80%] h-[30px] text-center'>{quizName}</h5>
+        <div className='h-[90%] w-[250px] bg-appleGray6 relative top-[40px] left-[50px] flex flex-col justify-between items-center pt-[25px] pb-[50px] rounded-[25px] shadow-custom'>
+            <h5 className='w-[80%] h-[101px] text-center flex justify-center items-center bg-white h-[60px] rounded-[20px] shadow-custom'>
+                <div>
+                    {quizName}
+                </div>
+            </h5>
             <div className='w-[85%] h-[100px] border-t-[2px] border-b-[2px] border-solid border-appleGray5 flex flex-row flex-wrap justify-center items-center gap-x-[10px] text-[15px]'>
-                <div className='h-[25px] w-[25px] bg-appleBlue rounded-[10px]'></div>
+                <div className='h-[25px] w-[25px] bg-appleBlue rounded-[10px] shadow-custom'></div>
                 <p className='pt-[15px] ml-[-6px]'>Current</p>
-                <div className='h-[25px] w-[25px] rounded-[10px] border-[2px] border-dashed'></div>
+                <div className='h-[25px] w-[25px] rounded-[10px] border-[2px] border-dashed shadow-custom'></div>
                 <p className='pt-[15px] ml-[-6px]'>Unanswered</p>
                 <div>
-                    <svg className='h-[25px] w-[25px] fill-red-1000 opacity-70' xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path d="M5 21V5q0-.825.588-1.412T7 3h10q.825 0 1.413.588T19 5v16l-7-3z"></path></svg>
+                    <svg className='h-[25px] w-[25px] fill-appleRed' xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path d="M5 21V5q0-.825.588-1.412T7 3h10q.825 0 1.413.588T19 5v16l-7-3z"></path></svg>
                 </div>
                 <p className='pt-[15px] ml-[-8px]'>Flagged</p>
             </div>
@@ -55,28 +59,28 @@ export default function QuestionNavBar({ review, questions, quizName }) {
                 {questions.map((_, index) => (
                     <div
                         key={index}
-                        className={`h-[40px] w-[40px] text-center text-appleBlue cursor-pointer rounded-[12px] pt-[5px]
+                        className={`h-[40px] w-[40px] text-center text-appleBlue cursor-pointer rounded-[12px] pt-[5px] shadow-custom
                             ${checkSelected(index) ? 'transform scale-[1.05] bg-appleBlue text-white' : 'bg-white text-appleBlue'}
                             ${!checkAnswered(index) ? 'border-[3px] border-dashed' : 'pt-[8px]'}
                         `}
                         style={{ transition: 'transform 0.3s ease-in-out' }}
                         onClick={() => handleKeyChange(index)}
                     >
-                        {index + 1}
                         {checkFlagged(index) ?
-                            <div className='relative top-[-5px] right-[-10px]'>
-                                <svg className='h-[25px] w-[25px] fill-red-1000 opacity-70' xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path d="M5 21V5q0-.825.588-1.412T7 3h10q.825 0 1.413.588T19 5v16l-7-3z"></path></svg>
+                            <div className='relative top-[-15px] left-[-10px] mb-[-25px]'>
+                                <svg className='h-[25px] w-[25px] fill-appleRed' xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path d="M5 21V5q0-.825.588-1.412T7 3h10q.825 0 1.413.588T19 5v16l-7-3z"></path></svg>
                             </div>
                         : null}
+                        {index + 1}
                     </div>
                 ))}
             </div>
             {!review ? 
                 <div id='buttons' className='mb-[10px] h-[100px] flex flex-col justify-around items-center'>
-                    <Button className='w-[150px] bg-appleBlue rounded-[20px] text-white'>
+                    <Button className='w-[150px] bg-appleBlue rounded-[20px] text-white shadow-custom'>
                         Pause Timer
                     </Button>
-                    <Button className='w-[150px] text-appleBlue rounded-[20px] bg-white border-[1px] border-appleBlue'>
+                    <Button className='w-[150px] text-appleBlue rounded-[20px] bg-white border-[1px] border-appleBlue shadow-custom'>
                         Submit Quiz
                     </Button>
                 </div> : null
--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,10 @@ const config = {
         appleGray6: '#F2F2F7',
         appleGray6Dark: '#1C1C1E',
         appleBlue: '#007AFF',
+        appleRed: '#FF3B30',
+      },
+      boxShadow: {
+        custom: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -4px rgba(0, 0, 0, 0.1)',
       },
     },
   },
